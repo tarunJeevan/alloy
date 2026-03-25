a73f117608496a9ba069a99da9794a72a1913671
Tarun Jeevan <tjeevan200@gmail.com>
2026-03-24 21:42:06 -0500

    Create links structs for link handling.

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod config;
 pub mod document;
 pub mod errors;
+pub mod links;
 pub mod modes;
 pub mod search;
 
 pub use document::Document;
 pub use errors::CoreError;
+pub use links::{Link, LinkIndex, LinkTarget, normalize_anchor};
 pub use modes::EditorMode;
 pub use search::{LARGE_DOC_THRESHOLD_BYTES, Match, SearchKind, SearchState};
